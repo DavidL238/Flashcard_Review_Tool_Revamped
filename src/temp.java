b2675b1b0351c0e2e9bd40b7fa53fe10a8bf1ae0
BT_1E10_30 <BT_1E10_30@bths.edu>
2022-05-09 10:42:05 -0400

    A

--- src/temp.java
+++ src/temp.java
@@ -5,16 +5,37 @@ import java.util.*;
 public class temp {
     public static void main(String[] args) throws Exception{
         URL webURL = new URL("https://quizlet.com/29679191/civil-war-flash-cards/");
-        URLConnection urlConnect = (URLConnection)webURL.openConnection();
+        URLConnection urlConnect = webURL.openConnection();
+        urlConnect.addRequestProperty("User-Agent", "Mozilla/4.76");
         InputStreamReader input = new InputStreamReader(urlConnect.getInputStream());
         BufferedReader bR = new BufferedReader(input);
-        ArrayList<String> divList = new ArrayList<>();
-        while (true) {
-            boolean isNeeded = false;
+        ArrayList<String> termList = new ArrayList<>();
+        ArrayList<String> definitionList = new ArrayList<>();
+        int termOrDefinition = 1;
+        boolean b = true;
+        while (b) {
             String line;
             if ((line = bR.readLine()) != null) {
-                if (line.contains(''))
+                int idxStart = line.indexOf("<span class=\"TermText notranslate lang-en\">");
+                if (idxStart != -1) {
+                    int idxEnd = line.indexOf("</span>");
+                    line = line.substring(idxStart + 1, idxEnd);
+                }
+                if (termOrDefinition % 2 == 1) {
+                    termList.add(line);
+                }
+                else {
+                    definitionList.add(line);
+                }
+                termOrDefinition++;
             }
+            else {
+                b = false;
+            }
+        }
+        for (int i = 0; i < termList.size(); i++) {
+            System.out.println(termList.get(i));
+            System.out.println(definitionList.get(i));
         }
     }
 }

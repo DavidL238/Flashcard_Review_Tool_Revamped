688490f872dc9d55f87b2b5983fe0a561fd89b11
BT_1E10_23 <sample@bths.edu>
2022-05-16 12:29:04 -0400

    Implemented JCheckBox & Added Dark Theme

--- src/StudyToolUI.java
+++ src/StudyToolUI.java
@@ -1,22 +1,60 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class StudyToolUI implements ActionListener {
-    public static void main(String[] args) {
-        JFrame uI = new JFrame();
+    private JCheckBox darkTheme;
+    private JFrame uI;
+    private JPanel darkPanel, backgroundPanel;
+    private Color dark, light;
+    private boolean isDark;
+
+    public StudyToolUI() {
+        isDark = false;
+        dark = new Color(12,12,12);
+        light = new Color(255,255,255);
+        uI = new JFrame();
         uI.setTitle("Generic Study Tool");
         uI.setVisible(true);
         uI.setSize(1280, 720);
-        uI.setResizable(false);
         uI.setLocationRelativeTo(null);
+        darkPanel = new JPanel();
+        backgroundPanel = new JPanel();
+        backgroundPanel.setVisible(true);
+        backgroundPanel.setSize(uI.getWidth(), uI.getHeight());
+        darkTheme = new JCheckBox("Dark Theme");
+        darkTheme.setVisible(true);
+        darkPanel.add(darkTheme);
+        darkTheme.addActionListener(this);
+        darkPanel.setVisible(true);
+        darkPanel.setSize(50, 50);
+        darkPanel.setBounds(250, 250, 200, 250);
+        uI.getContentPane().add(darkPanel);
+        uI.getContentPane().add(backgroundPanel);
         uI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JCheckBox darkCheck = new JCheckBox("Dark Mode");
-        uI.add(darkCheck);
-        darkCheck.setVisible(true);
-        darkCheck.setActionCommand("disable");
-        darkCheck.addActionListener(e -> uI.getContentPane().setBackground(new Color(12,12,12)));
+        darkTheme.setFocusPainted(false);
     }
 
-    public void checkTheme() {}
+    public void checkTheme() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == darkTheme) {
+            if (!isDark) {
+                darkTheme.setBackground(dark);
+                darkTheme.setForeground(light);
+                backgroundPanel.setBackground(dark);
+            }
+            else {
+                darkTheme.setBackground(light);
+                darkTheme.setForeground(dark);
+                backgroundPanel.setBackground(light);
+            }
+            isDark = !isDark;
+            // Implement saving
+        }
+    }
 }
--- src/a.java
+++ src/a.java
@@ -4,12 +4,6 @@ import java.util.*;
 
 public class a {
     public static void main(String[] args) throws Exception{
-        String a = "<span class=\"TermText notranslate lang-en\">";
-        int c = 0;
-        for (int i = 0; i < a.length(); i++) {
-            a = a.substring(0, a.length()-1);
-            c++;
-        }
-        System.out.println(c);
+        StudyToolUI a = new StudyToolUI();
     }
 }
